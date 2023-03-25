dd8a6825024a20a0858873f7a102dfafd0daa1f0
Sugiman <88069917+supersugiman@users.noreply.github.com>
2023-03-26 01:54:31 +0700

    Manifest

---
+++ Manifest.java
@@ -0,0 +1,9 @@
+package io.kodular.supersugiman.SIMPELSP2020LANJUTAN;
+/* loaded from: classes.dex */
+public final class Manifest {
+
+    /* loaded from: classes.dex */
+    public static final class permission {
+        public static final String MAPS_RECEIVE = "io.kodular.supersugiman.SIMPELSP2020LANJUTAN.permission.MAPS_RECEIVE";
+    }
+}
